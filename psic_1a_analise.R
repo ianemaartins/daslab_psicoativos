8ce73d2e8510055d04d3ede7e4c32291a560f77e
Iane Martins <ianemartins99@gmail.com>
2024-06-23 16:48:28 -0300

    comecei a fazer a relação entre os dados totais e os dados por psicoativos

--- psic_1a_analise.R
+++ psic_1a_analise.R
@@ -312,3 +312,66 @@ ggsave("grafico_raca.png", grafico_raca, width = 10, height = 6, dpi = 300)
 ggsave("grafico_mortes_por_ano.png", grafico_mortes_por_ano, width = 10, height = 6, dpi = 300)
 ggsave("grafico_escolaridade.png", grafico_escolaridade, width = 10, height = 6, dpi = 300)
 ggsave("grafico_estado_civil.png", grafico_estado_civil, width = 10, height = 6, dpi = 300)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+#--------------RELAÇÃO ENTRE OS DADOS TOTAIS E OS DADOS POR PSICOATIVOS --------------
+
+#juntando os dados totais x e os dados filtrados segundo as CIDs y de cada variavel
+
+# MORTES POR ANO
+uniao_ano_obito <- merge(frequencias_ano_obito, frequenciasf_ano_obito, by = "ANOOBITO") #juntando os dados
+
+uniao_ano_obito <- uniao_ano_obito %>% #mortes por psicoativos a cada 1000 mortes totais
+  mutate(Mortes_por_1000 = (Quantidade.y / Quantidade.x) * 1000)
+
+# GENERO
+uniao_genero <- merge(frequencias_genero, frequenciasf_genero, by = "SEXO") #juntando os dados
+
+uniao_genero <- uniao_genero %>% #mortes por psicoativos a cada 1000 mortes totais
+  mutate(Mortes_por_1000 = (Quantidade.y / Quantidade.x) * 1000)
+
+#ESCOLARIDADE
+uniao_escolaridade <- merge(frequencias_escolaridade, frequenciasf_escolaridade, by = "ESC") #juntando os dados
+
+#ESTADO CIVIL
+uniao_estado_civil <- merge(frequencias_estado_civil, frequenciasf_estado_civil, by = "ESTCIV") #juntando os dados
+
+#RACA
+uniao_raca <- merge(frequencias_raca, frequenciasf_raca, by = "RACACOR") #juntando os dados
+
+#FAIXA ETARIA
+uniao_faixa_etaria <- merge(frequencias_faixa_etaria, frequenciasf_faixa_etaria, by = "FAIXAETA") #juntando os dados
+
+#IDADE
+# Calcular a média
+media_idade_total <- mean(dados2$IDADE2, na.rm = TRUE)
+media_idade_psicoativos <- mean(dados_filtrados$IDADE2, na.rm = TRUE)
+
+# Calcular a mediana
+mediana_idade_total <- median(dados2$IDADE2, na.rm = TRUE)
+mediana_idade_psicoativos <- median(dados_filtrados$IDADE2, na.rm = TRUE)
+
+#boxplot
+
+ggplot(data = dados2, aes(y = IDADE2)) + 
+  geom_boxplot() +
+  labs(title = "Boxplot de Idades de Mortes por Uso de Psicoativos",
+       y = "Idade",
+       x = "") +
+
+
+
